56339d21f613eebf83696f9d8cd2b15d9f29930a
J200W <james.wellington@efrei.net>
2023-06-23 10:09:25 +0200

    Update MainPage.vue

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -9,12 +9,6 @@ const new_books = JSON.parse(sessionStorage.getItem('new_books'))
 const current_books = JSON.parse(sessionStorage.getItem('current_books'))
 const discover_books = JSON.parse(sessionStorage.getItem('discover_books'))
 
-        {
-            id: 3,
-            src: require("@/assets/onepiece98.png"),
-            title: "One Piece 98",
-        },
-
 var connected = true;
 
 
