d1cdafaebb8e14a9bdbf6cf358ffa3f073cff939
maxim <maxime.goujon@efrei.net>
2023-06-27 10:51:17 +0200

    BorrowBook, style : V2, js : V1

--- frontend/src/views/BorrowBook.vue
+++ frontend/src/views/BorrowBook.vue
@@ -1,58 +1,58 @@
 <script setup>
-    import NavbarConnected from "@/components/NavbarConnected.vue";
+
+import NavbarConnected from "@/components/NavbarConnected.vue";
     import NavbarNonConnected from "@/components/NavbarNonConnected.vue";
     import TheFooter from '@/components/TheFooter.vue'
-    import MyEbooksContent from "@/components/MyEbooksContent.vue";
 
-    const book = [
-    {
-        id: 1,
-        title: "One Piece Tome 96",
-        src: require("@/assets/onepiece96.png"),
-        author: "Eiichiro Oda",
-        date: "04/11/2020",
-        library: "Bibliothèque de l'Université de Lille",
-        time: "34d 12h 32m",
-    },
-    ]
+    const books = JSON.parse(sessionStorage.getItem('similar_books'));
 
-    function validateDate() {
-      var selectedDate = new Date(document.getElementById('dateInput').value);
-      var maxDate = new Date();
-      maxDate.setDate(maxDate.getDate() + 21); // Ajoute 21 jours à la date actuelle
+    var link = window.location.href;
+    // Get the id of the book from the url
 
-      if (selectedDate > maxDate) {
-        alert("La date saisie doit être postérieure de moins de 3 semaines à la date actuelle.");
-        return false;
-      }
+    var book_id = link.split("?id=").pop();
 
-      return true;
-    }
+    var book_list = sessionStorage.getItem('book_list');
+
+    book_list = JSON.parse(book_list);
+
+    const book = book_list[book_id - 1];
+
+
     var connected = true;
+    var previousUrl = document.referrer;
+    previousUrl = previousUrl.split("?id=")[0]
 </script>
 
 <template>
     <NavbarConnected v-if="connected" />
     <NavbarNonConnected v-if="!connected" />
     <body>
-          <MyEbooksContent :books="book" />
+    <img id="bookImg" :src="book.src" alt="{{ book.title }}" >
           <div class="form-container">
-            <h2>Entrez la date de fin d'emprunt</h2>
+            <h2 class ="titre">Entrez la date de fin d'emprunt</h2>
             <div class = "form-group">
-            <form onsubmit="return validateDate()">
+            <form onsubmit="validateDate()">
               <input type="date" id="dateInput" required>
             </form>
             </div>
-            <input type="submit" value="Soumettre" id="bouton">
+            <input type="submit" value="Soumettre" id="bouton" onclick="
+
+            var selectedDate = new Date(document.getElementById('dateInput').value);
+            var maxDate = new Date();
+            maxDate.setDate(maxDate.getDate() + 21);
+            if (selectedDate > maxDate || selectedDate < new Date()) {
+               alert('La date saisie doit être postérieure de moins de 3 semaines à la date actuelle.');
+               return false;
+            }
+            return true;">
+
           </div>
 
     </body>>
     
     <TheFooter />
 </template>
-  
-  
-  
+
 <script>
 
 export default {
@@ -63,6 +63,11 @@ export default {
 </script>
 
 <style>
+
+.titre{
+  position: center;
+  margin-left: 525px;
+}
 .form-container {
   max-width: 1400px;
   margin: 0 auto;
