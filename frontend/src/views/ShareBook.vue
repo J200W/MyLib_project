bf7ada035bada5c02169bd177d2a6f25d1457e42
Ethan <ethansuissa@gmail.com>
2023-06-30 21:14:03 +0200

    Mise en place Share: livre chargé en dur mais share possible dans bdd, css aménagé pour bouton

--- frontend/src/views/ShareBook.vue
+++ frontend/src/views/ShareBook.vue
@@ -3,8 +3,9 @@
     import NavbarNonConnected from "@/components/NavbarNonConnected.vue";
     import TheFooter from "@/components/TheFooter.vue";
     import Share from "@/components/Share.vue"
-    var connected = sessionStorage.getItem('connected');
+    import {port} from "../../../backend/controllers/Tools_controllers";
 
+    var connected = sessionStorage.getItem('connected');
     if (connected == null) {
         connected = false;
     }
@@ -14,7 +15,9 @@
 <template>
   <NavbarConnected v-if="connected" />
   <NavbarNonConnected v-if="!connected" />
-  <Share />
+  <Share :book_shared="book_shared"
+         :emprunt_dates="emprunt_dates"
+         :stock="stock" />
   <TheFooter />
 </template>
 
@@ -26,9 +29,63 @@
 <script>
 export default {
     name: 'ShareBook',
-    data() { return {} },
-    methods: {
+  mounted() {
+    this.fetchUserData();
+  },
+    data() {
+    return {
+      book_shared: {},
+      emprunt_dates: {},
+      stock: -1,
+    };
+  },
 
-    }
+  methods: {
+    fetchUserData() {
+
+      // TEMPORAIRE
+      sessionStorage.setItem("book_detailed",
+          JSON.stringify({
+        id: 53,
+        title: "One Piece 96",
+        src: "onepiece96.png",
+        author: "Eiichiro Oda",
+        library: "Bibliothèque de l'Université de Lille",
+        theme: "Manga1",
+        genre: "Shonen",
+        date: "2020-12-16",
+        edition: "Glénat",
+        language: "Français",
+        pdf: "onepiece96.pdf",
+        pages: 192,
+        description:
+            "Luffy et ses compagnons sont enfin arrivés sur Onigashima, où se déroule le plus grand festival du monde ! Mais pour infiltrer le pays des samouraïs, il leur faut un passeport, et pour l’obtenir, il leur faut affronter les gardiens de la porte des enfers !",
+      }));
+
+    sessionStorage.setItem("book_detailed_emprunt_dates",
+        JSON.stringify({
+          debut_emprunt: "2021-05-01",
+          fin_emprunt: "2021-05-08",
+        }));
+
+    sessionStorage.setItem("book_detailed_stock", "1");
+
+    this.book_shared = JSON.parse(sessionStorage.getItem("book_detailed"));
+    this.emprunt_dates = JSON.parse(sessionStorage.getItem("book_detailed_emprunt_dates"));
+    this.stock = +sessionStorage.getItem("book_detailed_stock");
+
+  }
+    // Effectuer une requête HTTP vers la page PHP pour récupérer les données utilisateur
+    /*
+      fetch("http://localhost:" + port +"/elements_to_send.php")
+          .then(response => response.json())
+          .then(data => {
+            console.log('Données utilisateur reçues:', data);
+
+          .catch(error => {
+            console.error('Erreur lors de la récupération des données utilisateur:', error);
+          });
+    },*/
+  }
 }
 </script>
